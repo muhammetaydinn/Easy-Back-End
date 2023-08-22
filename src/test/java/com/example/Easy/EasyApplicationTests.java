19cf7b39a7ff2a8819ad9846ab23b8984f583269
OmerI53 <omer.islam53@hotmail.com>
2023-08-22 09:58:53 +0300

    first push

---
+++ src/test/java/com/example/Easy/EasyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Easy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
